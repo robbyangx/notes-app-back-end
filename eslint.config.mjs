26ebf353219d70f2dbdfe98707beeb6529aa0df9
Robbyan <robbyan.g1@gmail.com>
2024-05-04 16:01:24 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  "rules",
+  {
+    "no-console": "off",
+  },
+];
